a996315302cb6f5267ce7f7bcba0a09333077c59
A <inspiration3@gmail.com>
2009-10-25 13:06:31 +0000

    Added C# generator for debugging purposes.

---
+++ Scripting/CSharpGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.CSharp;
+
+namespace IronAHK.Scripting
+{
+    partial class IACodeProvider
+    {
+        [Conditional("DEBUG")]
+        void PrintCSharpCode(CodeCompileUnit[] units, TextWriter write)
+        {
+            var provider = new CSharpCodeProvider();
+            var options = new CodeGeneratorOptions { BracingStyle = "C", ElseOnClosing = false, IndentString = "\t" };
+            foreach (CodeCompileUnit code in units)
+            {
+                try { provider.GenerateCodeFromCompileUnit(code, write, options); }
+                catch (Exception e) { write.WriteLine(e.Message); }
+                finally { }
+            }
+        }
+    }
+}
