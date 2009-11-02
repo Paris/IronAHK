79ce3aeb2a2e395c40077c9a50c7dfb5b9ba094c
A <inspiration3@gmail.com>
2009-11-02 11:55:20 +0000

    Removed make dist in batch file for now.

--- make.bat
+++ make.bat
@@ -9,21 +9,3 @@ CALL "%VS90COMNTOOLS%\vsvars32.bat"
 SET name=IronAHK
 devenv /rebuild "Release|Any CPU" "%name%.sln"
 
-SET Sz=%ProgramFiles%\7-Zip\7z.exe
-IF NOT EXIST "%Sz%" (
-	ECHO Could not find 7-zip.
-	EXIT 1
-)
-
-SET version=
-FOR /F %%G IN (version.txt) DO SET version=%%G
-
-SET cwd=%CD%
-SET release=Release
-CD "%name%\bin"
-MOVE "%release%" "%name%"
-SET out=%name%\%name%-%version%.zip
-IF EXIST "%out%" DEL "%out%"
-"%Sz%" a "%out%"  "%name%" "-x!%out%" -x!*.zip -mx=9
-MOVE "%name%" "%release%"
-CD "%cwd%"
