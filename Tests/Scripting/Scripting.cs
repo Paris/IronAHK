615921921c8e652c043d7c0b63df61889b34e302
A <inspiration3@gmail.com>
2009-11-12 16:04:26 +0000

    Changed NUnit tests.

--- Tests/Scripting/Scripting.cs
+++ Tests/Scripting/Scripting.cs
@@ -1,55 +1,36 @@
-﻿using System;
-using System.CodeDom.Compiler;
+﻿using System.CodeDom.Compiler;
 using System.IO;
-using System.Text;
 using IronAHK.Scripting;
 using NUnit.Framework;
 
-namespace Tests
+namespace IronAHK.Tests
 {
     [TestFixture]
-    public partial class Scripting
+    public class Scripting
     {
         [Test]
         public void RunScripts()
         {
             string path = string.Format("..{0}..{0}Scripting{0}Code", Path.DirectorySeparatorChar.ToString());
 
-            foreach (string file in Directory.GetFiles(path, "*.ia"))
+            var sources = new StreamReader(Path.Combine(path, "order.txt"));
+            string line;
+
+            while ((line = sources.ReadLine()) != null)
             {
-                string name = Path.GetFileNameWithoutExtension(file);
+                if (line == "!")
+                    break;
+                
+                string file = Path.Combine(path, line + ".ia");
                 var provider = new IACodeProvider();
 
                 var options = new CompilerParameters();
-                options.GenerateExecutable = true;
-                options.GenerateInMemory = false;
-                options.ReferencedAssemblies.Add(typeof(IronAHK.Rusty.Core).Namespace + ".dll");
-                options.OutputAssembly = Path.GetTempFileName() + ".exe";
+                options.GenerateExecutable = false;
+                options.GenerateInMemory = true;
+                options.ReferencedAssemblies.Add(typeof (IronAHK.Rusty.Core).Namespace + ".dll");
 
-                provider.CompileAssemblyFromFile(options, file);
-
-                bool exists = File.Exists(options.OutputAssembly);
-                Assert.IsTrue(exists, name + " assembly");
-
-                if (exists)
-                {
-                    var buffer = new StringBuilder();
-                    var writer = new StringWriter(buffer);
-                    Console.SetOut(writer);
-
-                    AppDomain.CurrentDomain.ExecuteAssembly(options.OutputAssembly);
-
-                    try { File.Delete(options.OutputAssembly); }
-                    catch (UnauthorizedAccessException) { }
-
-                    writer.Flush();
-                    string output = buffer.ToString();
-                    Assert.AreEqual("pass", output, name);
-
-                    var stdout = new StreamWriter(Console.OpenStandardOutput());
-                    stdout.AutoFlush = true;
-                    Console.SetOut(stdout);
-                }
+                var results = provider.CompileAssemblyFromFile(options, file);
+                results.CompiledAssembly.EntryPoint.Invoke(null, null);
             }
         }
     }
