957b31e7de6b89dbae871dd7b81fe082a120f754
A <inspiration3@gmail.com>
2009-11-18 11:11:52 +0000

    Tidied code.

--- Tests/Scripting/Scripting.cs
+++ Tests/Scripting/Scripting.cs
@@ -12,9 +12,8 @@ namespace IronAHK.Tests
         public void RunScripts()
         {
             string path = string.Format("..{0}..{0}Scripting{0}Code", Path.DirectorySeparatorChar.ToString());
-
-            var sources = new StreamReader(Path.Combine(path, "order.txt"));
             string line;
+            var sources = new StreamReader(Path.Combine(path, "order.txt"));
 
             while ((line = sources.ReadLine()) != null)
             {
@@ -24,10 +23,8 @@ namespace IronAHK.Tests
                 string file = Path.Combine(path, line + ".ia");
                 var provider = new IACodeProvider();
 
-                var options = new CompilerParameters();
-                options.GenerateExecutable = false;
-                options.GenerateInMemory = true;
-                options.ReferencedAssemblies.Add(typeof (IronAHK.Rusty.Core).Namespace + ".dll");
+                var options = new CompilerParameters { GenerateExecutable = false, GenerateInMemory = true, };
+                options.ReferencedAssemblies.Add(typeof(IronAHK.Rusty.Core).Namespace + ".dll");
 
                 var results = provider.CompileAssemblyFromFile(options, file);
                 results.CompiledAssembly.EntryPoint.Invoke(null, null);
