6972688b89ebd497b7fdce4537985b33d87c3b4a
polyethene <poly@ethene.net>
2009-11-24 23:28:49 +0000

    Enabled NUnit testing of scripts.

--- Tests/Scripting/Scripting.cs
+++ Tests/Scripting/Scripting.cs
@@ -1,5 +1,7 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
+using System.CodeDom.Compiler;
 using System.IO;
+using System.Text;
 using IronAHK.Scripting;
 using NUnit.Framework;
 
@@ -19,15 +21,32 @@ namespace IronAHK.Tests
             {
                 if (line == "!")
                     break;
-                
+
                 string file = Path.Combine(path, line + ".ia");
                 var provider = new IACodeProvider();
-
                 var options = new CompilerParameters { GenerateExecutable = false, GenerateInMemory = true, };
                 options.ReferencedAssemblies.Add(typeof(IronAHK.Rusty.Core).Namespace + ".dll");
-
                 var results = provider.CompileAssemblyFromFile(options, file);
+
+                var buffer = new StringBuilder();
+                var writer = new StringWriter(buffer);
+                Console.SetOut(writer);
+
                 results.CompiledAssembly.EntryPoint.Invoke(null, null);
+
+                writer.Flush();
+                string output = buffer.ToString();
+                var stdout = new StreamWriter(Console.OpenStandardOutput());
+                stdout.AutoFlush = true;
+                Console.SetOut(stdout);
+
+                Assert.IsNotEmpty(output, line + " results");
+
+                const string pass = "pass";
+                foreach (string remove in new string[] { pass, " ", "\n" })
+                    output = output.Replace(remove, string.Empty);
+
+                Assert.IsEmpty(output, line + " " + pass);
             }
         }
     }
