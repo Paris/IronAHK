ef8f60c9c76065e704dbf221ab5214b6c8331686
polyethene <poly@ethene.net>
2009-12-28 15:29:20 +0000

    Moved dynamic function call method from Rusty to Script class.

---
+++ Scripting/Script/Call.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace IronAHK.Scripting
+{
+    partial class Script
+    {
+        public static object FunctionCall(string name, params object[] parameters)
+        {
+            var stack = new StackTrace(false).GetFrames();
+            MethodInfo method = null;
+
+            for (int i = 0; i < 3; i++)
+            {
+                var type = stack[i].GetMethod().DeclaringType;
+                method = FindMethod(name, type.GetMethods(), parameters);
+                if (method != null)
+                    break;
+            }
+
+            return method == null || !method.IsStatic ? null : method.Invoke(null, new object[] { parameters });
+        }
+
+        static MethodInfo FindMethod(string name, MethodInfo[] list, object[] parameters)
+        {
+            for (int i = 0; i < list.Length; i++)
+                if (list[i].Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                    return list[i];
+
+            return null;
+        }
+    }
+}
