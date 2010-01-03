585f2cf41ac192803d36e4d6d6345f465c43e963
A <inspiration3@gmail.com>
2010-01-03 10:26:33 +0000

    Combined Windows batch scripts.

--- make.bat
+++ make.bat
@@ -1,10 +1,26 @@
 @ECHO OFF
 
+SET cmd=%1
+IF "%cmd%"=="" SET cmd=all
+GOTO %cmd%
+
+:all
+CALL :setvars
+devenv /rebuild "Release|Any CPU" "%name%.sln"
+GOTO :eof
+
+:setvars
 IF NOT DEFINED VS90COMNTOOLS (
 	ECHO Visual Studio 9.0 is not installed.
 	EXIT 1
 )
 CALL "%VS90COMNTOOLS%\vsvars32.bat"
-
 SET name=IronAHK
-devenv /rebuild "Release|Any CPU" "%name%.sln"
+GOTO :eof
+
+:clean
+FOR /D %%G IN ("*") DO (
+	RMDIR /Q /S "%%G\bin" 2>NUL
+	RMDIR /Q /S "%%G\obj" 2>NUL
+)
+GOTO :eof
