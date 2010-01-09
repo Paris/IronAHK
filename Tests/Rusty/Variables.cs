68dcb0171245bba71fd7943049bd505c22fc34b7
polyethene <poly@ethene.net>
2010-01-09 15:15:03 +0000

    Added variables NUnit tests.

---
+++ Tests/Rusty/Variables.cs
@@ -0,0 +1,22 @@
+﻿using IronAHK.Rusty;
+using NUnit.Framework;
+
+namespace IronAHK.Tests
+{
+    partial class Rusty
+    {
+        [Test, Category("Variables")]
+        public void Variables()
+        {
+            const string value = "qwerty";
+            Core.SetEnv("AbCd", value);
+            Assert.AreEqual(value, Core.GetEnv("abcd"), "Case insensitive variable reference");
+        }
+
+        [Test, Category("Variables")]
+        public void ReservedVariables()
+        {
+            Assert.IsTrue((int)Core.GetEnv("a_tickCount") > 0);
+        }
+    }
+}
