1fd57101a72bcd287affbbcd62c6de675d62aa83
A <inspiration3@gmail.com>
2010-02-07 09:38:21 +0000

    Ignore comments in continuation line expression statements.

---
+++ Scripting/Parser/Texts/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IronAHK.Scripting
+{
+    partial class Parser
+    {
+        void MoveToEOL(string code, ref int i)
+        {
+            while (i < code.Length)
+            {
+                switch (code[i])
+                {
+                    case CR:
+                        int n = i + 1;
+                        if (n < code.Length && code[n] == LF)
+                            i = n;
+                        goto case LF;
+
+                    case LF:
+                        return;
+
+                    default:
+                        i++;
+                        break;
+                }
+            }
+        }
+    }
+}
