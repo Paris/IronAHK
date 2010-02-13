6d372e30ff4ee755318ee5cfaab6563b54318fb3
Tobias Kappé <tobias@ntlabs.org>
2010-02-13 15:00:20 +0100

    Rewrote method call code to be clearer

--- Scripting/Script/Parameters.cs
+++ Scripting/Script/Parameters.cs
@@ -3,7 +3,7 @@ namespace IronAHK.Scripting
 {
     partial class Script
     {
-        public static void Parameters(string[] names, object[] values, object[] defaults)
+        public static void Parameters(string[] names, object[] values, string[] defaults)
         {
             for (int i = 0; i < names.Length; i++)
             {
