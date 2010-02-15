f19dd53e13c8375ba2fe215fe2208b74af2279c4
polyethene <poly@ethene.net>
2010-02-15 16:20:40 +0000

    More generator support.

---
+++ Scripting/Generator/Emission/Flow.cs
@@ -0,0 +1,72 @@
+﻿using System.CodeDom;
+
+namespace IronAHK.Scripting
+{
+    partial class Emit
+    {
+        void EmitConditionStatement(CodeConditionStatement cond)
+        {
+            WriteSpace();
+            writer.Write("if (");
+            EmitExpression(cond.Condition);
+            writer.Write(")");
+            WriteSpace();
+            writer.Write("{");
+            depth++;
+            EmitStatements(cond.TrueStatements);
+            depth--;
+            WriteSpace();
+            writer.Write("}");
+            if (options.ElseOnClosing)
+                writer.Write(" ");
+            else
+                WriteSpace();
+            writer.Write("else");
+            if (options.ElseOnClosing)
+                writer.Write(" ");
+            else
+                WriteSpace();
+            writer.Write("{");
+            depth++;
+            EmitStatements(cond.FalseStatements);
+            depth--;
+            WriteSpace();
+            writer.Write("}");
+        }
+
+        void EmitIteration(CodeIterationStatement iteration)
+        {
+            EmitStatement(iteration.InitStatement);
+            WriteSpace();
+            writer.Write("white (");
+
+            depth++;
+            EmitExpression(iteration.TestExpression);
+            depth--;
+
+            writer.Write(')');
+            WriteSpace();
+            writer.Write('{');
+
+            depth++;
+            EmitStatements(iteration.Statements);
+            EmitStatement(iteration.IncrementStatement);
+            depth--;
+
+            WriteSpace();
+            writer.Write('{');
+        }
+
+        void EmitGoto(CodeGotoStatement go)
+        {
+            writer.Write("goto ");
+            writer.Write(go.Label);
+        }
+
+        void EmitLabel(CodeLabeledStatement label)
+        {
+            writer.Write(label.Label);
+            writer.Write(':');
+        }
+    }
+}
---
+++ Scripting/Generator/Emission/Members.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+
+namespace IronAHK.Scripting
+{
+    partial class Emit
+    {
+        void EmitMethod(CodeMemberMethod method)
+        {
+            WriteSpace();
+
+            writer.Write(method.Name);
+
+            bool first = true;
+            foreach (CodeParameterDeclarationExpression param in method.Parameters)
+            {
+                if (!first)
+                    writer.Write(" ");
+
+                switch (param.Direction)
+                {
+                    case FieldDirection.Out:
+                        throw new NotSupportedException();
+
+                    case FieldDirection.Ref:
+                        writer.Write("ByRef ");
+                        break;
+                }
+
+                writer.Write(param.Name);
+
+                if (first)
+                    first = false;
+                else
+                    writer.Write(",");
+
+            }
+
+            depth++;
+            EmitStatements(method.Statements);
+            depth--;
+        }
+    }
+}
