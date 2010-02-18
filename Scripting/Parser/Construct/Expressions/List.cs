8d16e7027addd7d4f5563fad9c1c6e218abd8867
polyethene <poly@ethene.net>
2010-02-18 16:12:12 +0000

    Combined reusable code into methods.

---
+++ Scripting/Parser/Construct/Expressions/List.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace IronAHK.Scripting
+{
+    partial class Parser
+    {
+        List<object> Dissect(List<object> parts, int start, int end)
+        {
+            var extracted = new List<object>(end - start);
+
+            for (int i = start; i < end; i++)
+            {
+                extracted.Add(parts[start]);
+                parts.RemoveAt(start);
+            }
+
+            return extracted;
+        }
+    }
+}
