f41326d9fb1c5b541c9af9ad9082e1ac11d258a4
A <inspiration3@gmail.com>
2010-02-24 20:53:03 +0000

    Added make dist target on Windows.

--- make.bat
+++ make.bat
@@ -13,11 +13,14 @@ GOTO :eof
 :setvars
 CALL :vs
 SET name=IronAHK
+SET setup=Setup
 SET libname=Rusty
 SET config=Release
-SET outdir=bin
+SET outpre=bin
 SET site=Site
 SET devargs="%config%" "%name%.sln"
+SET versionfile=%name%\version.txt
+FOR /F %%G IN (%versionfile%) DO SET version=%%G
 GOTO :eof
 
 :vs
@@ -35,6 +38,26 @@ CALL :all
 php-cgi -f "%name%\%site%\transform.php"
 GOTO :eof
 
+:dist
+CALL %0 docs
+devenv /rebuild %devargs% /project "%setup%"
+IF NOT EXIST "%outpre%" MKDIR "%outpre%"
+ECHO *>"%outpre%\.gitignore"
+SET cwd=%CD%
+CD "%setup%\%outpre%\%config%"
+"%setup%.exe"
+FOR %%G IN (*.msi) DO MOVE "%%G" "%cwd%\%outpre%\%name%-%version%-%%G"
+CD "%cwd%"
+SET Sz=%ProgramFiles%\7-zip
+IF EXIST "%Sz%" SET PATH=%PATH%;%Sz%
+CD "%name%\%outpre%"
+SET outname=%name%-%version%
+MOVE "%config%" "%outname%"
+7z a "%cwd%\%outpre%\%name%-%version%.zip" "%outname%" -mx=9
+MOVE "%outname%" "%config%"
+CD "%cwd%"
+GOTO :eof
+
 :mostlyclean
 CALL :clean
 php-cgi -f "%name%\%site%\clean.php" > NUL
@@ -42,7 +65,8 @@ GOTO :eof
 
 :clean
 FOR /D %%G IN ("*") DO (
-	RMDIR /Q /S "%%G\%outdir%" 2>NUL
+	RMDIR /Q /S "%%G\%outpre%" 2>NUL
 	RMDIR /Q /S "%%G\obj" 2>NUL
 )
+IF EXIST "%outpre%" RMDIR /Q /S "%outpre%"
 GOTO :eof
