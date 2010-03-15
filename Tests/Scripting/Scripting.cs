c0d43030e9750a746cae8e0f505e48cdb5fabf3e
polyethene <poly@ethene.net>
2010-03-15 14:50:50 +0000

    Moved test scripts to parent folder so they are easier to find.

--- Tests/Scripting/Scripting.cs
+++ Tests/Scripting/Scripting.cs
@@ -10,7 +10,7 @@ namespace IronAHK.Tests
     [TestFixture, Category("Scripting")]
     public partial class Scripting
     {
-        string path = string.Format("..{0}..{0}Scripting{0}Code{0}", Path.DirectorySeparatorChar.ToString());
+        string path = string.Format("..{0}..{0}Code{0}", Path.DirectorySeparatorChar.ToString());
         const string ext = ".ahk";
 
         bool TestScript(string source)
