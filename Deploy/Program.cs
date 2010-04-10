8c5e18ea6e00eb4fe442b81cdb99e2522c88d025
A <inspiration3@gmail.com>
2010-04-10 13:40:56 +0100

    Renamed Setup project to Deploy.

---
+++ Deploy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+
+namespace IronAHK.Setup
+{
+    static partial class Program
+    {
+        static void Main(string[] args)
+        {
+            x64 = false;
+            Build();
+            x64 = true;
+            Build();
+
+#if DEBUG
+            Console.Read();
+#endif
+        }
+    }
+}
