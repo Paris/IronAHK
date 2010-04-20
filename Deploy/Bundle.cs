41e9224b4f84c8eb285ab8173dce178b3fdcb684
A <inspiration3@gmail.com>
2010-04-20 12:12:18 +0100

    Added stub methods for deployment.

---
+++ Deploy/Bundle.cs
@@ -0,0 +1,11 @@
+﻿
+namespace IronAHK.Setup
+{
+    partial class Program
+    {
+        static void AppBundle()
+        {
+            // TODO: create OSX app bundle
+        }
+    }
+}
---
+++ Deploy/Package.cs
@@ -0,0 +1,11 @@
+﻿
+namespace IronAHK.Setup
+{
+    partial class Program
+    {
+        static void PackageZip()
+        {
+            // TODO: create binary zip packages
+        }
+    }
+}
