212a9ca31c4613dd25cad7421f9fb20f45731d15
polyethene <poly@ethene.net>
2010-04-20 11:46:48 +0100

    Rewrote Windows installers.

--- Deploy/Installer.cs
+++ Deploy/Installer.cs
@@ -1,19 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Xml.Linq;
-using Microsoft.Win32;
-using WixSharp;
 
 namespace IronAHK.Setup
 {
     partial class Program
     {
-        static bool x64, ext;
-        static int n;
+        static int c;
+        static List<string> components;
 
-        static void Build()
+        static void BuildMsi()
         {
+            BuildMsi(false);
+            BuildMsi(true);
+            components = null;
+        }
+
+        static void BuildMsi(bool x64)
+        {
+            string xml = Path.GetTempFileName(), proj = Path.GetTempFileName();
+            var src = new StreamWriter(xml);
+            BuildMsi(src, x64);
+            src.Close();
+
+            string dir = string.Format("..{0}..{0}WiX", Path.DirectorySeparatorChar.ToString());
+
+            if (Directory.Exists(dir))
+                Environment.SetEnvironmentVariable("PATH", string.Concat(Environment.GetEnvironmentVariable("PATH"), Path.PathSeparator.ToString(), dir));
+
+            var wix = new Process { StartInfo = new ProcessStartInfo { UseShellExecute = false } };
+
+            wix.StartInfo.FileName = "candle";
+            wix.StartInfo.Arguments = string.Format("-arch x{2} -nologo -out \"{1}\" \"{0}\"", xml, proj, x64 ? "64" : "86");
+            wix.Start();
+            wix.WaitForExit();
+            File.Delete(xml);
+
+            string output = string.Format("x{0}.msi", x64 ? "64" : "86");
+
+            if (File.Exists(output))
+                File.Delete(output);
+
+            wix.StartInfo.FileName = "light";
+            wix.StartInfo.Arguments = string.Format("-nologo -sw2738 -ext WixUIExtension.dll \"{0}\" -out \"{1}\"", proj, output);
+            wix.Start();
+            wix.WaitForExit();
+            File.Delete(proj);
+        }
+
+        static void BuildMsi(TextWriter writer, bool x64)
+        {
+            c = 0;
+            components = new List<string>();
+            string upgrade = Guid;
+
             const string config =
 #if DEBUG
  "Debug"
@@ -22,122 +63,144 @@ namespace IronAHK.Setup
 #endif
 ;
 
+            const string author = "A";
             string name = typeof(Program).Namespace.Split(new char[] { '.' }, 2)[0];
             string path = Path.GetFullPath(string.Format("..{0}..{0}..{0}{1}{0}bin{0}" + config, Path.DirectorySeparatorChar, name));
-            string target = string.Format("%ProgramFiles{2}%{0}{1}", Path.DirectorySeparatorChar, name, x64 ? "64Folder" : string.Empty);
-            string version = System.IO.File.ReadAllText(string.Format("{1}{0}..{0}..{0}version.txt", Path.DirectorySeparatorChar, path)).Trim();
-            string docroot = Path.GetFullPath(string.Format("{1}{0}..{0}..{0}Site{0}docs", Path.DirectorySeparatorChar, path));
-            const string shortcut = "%ProgramMenu%";
+            string version = File.ReadAllText(string.Format("{1}{0}..{0}..{0}version.txt", Path.DirectorySeparatorChar, path)).Trim();
+            string docs = Path.GetFullPath(string.Format("{1}{0}..{0}..{0}Site{0}docs", Path.DirectorySeparatorChar, path));
+            string favicon = string.Format("{1}{0}..{0}favicon.ico", Path.DirectorySeparatorChar.ToString(), docs);
+            string main = name + ".exe";
+            const string license = "license.txt";
+            const string licenseRtf = "license.rtf";
             const string extension = "ia";
 
-            var project = new Project();
+            writer.WriteLine("<?xml version='1.0' encoding='utf-8'?>");
+            writer.WriteLine("<Wix xmlns='http://schemas.microsoft.com/wix/2006/wi' xmlns:netfx='http://schemas.microsoft.com/wix/NetFxExtension'>");
+            writer.WriteLine("  <Product Id='{0}' Name='{1}' Language='1033' Codepage='Windows-1252' Version='{2}' Manufacturer='{3}' UpgradeCode='{4}'>",
+                Guid, name, version, author, upgrade);
 
-            project.Name = name;
-            project.Version = new Version(version);
-            project.UI = WUI.WixUI_InstallDir;
-            project.MSIFileName = x64 ? "x64" : "x86";
-            project.AddRemoveProgramsIcon = string.Format("{1}{0}..{0}..{0}Site{0}favicon.ico", Path.DirectorySeparatorChar, path);
+            writer.WriteLine("    <Package InstallerVersion='200' Compressed='yes' Languages='1033' InstallScope='perMachine' Platform='x{0}' />", x64 ? "64" : "86");
+            writer.WriteLine("    <Media Id='1' Cabinet='{0}.cab' EmbedCab='yes' />", name);
+            writer.WriteLine("    <Directory Id='TARGETDIR' Name='SourceDir'>");
+            writer.WriteLine("      <Directory Id='ProgramFiles{0}Folder' Name='ProgramFiles{0}Folder'>", x64 ? "64" : string.Empty);
+            writer.WriteLine("        <Directory Id='INSTALLDIR' Name='{0}'>", name);
 
-            project.LicenceFile = string.Format("{1}{0}..{0}..{0}license.rtf", Path.DirectorySeparatorChar, path);
-            project.MajorUpgradeStrategy = MajorUpgradeStrategy.Default;
-
-            n = 0;
-            var docs = RecurseFolder(docroot);
-
-            project.Dirs = new[]
+            foreach (var dll in new[] { typeof(Rusty.Core), typeof(Scripting.IACodeProvider) })
             {
-                new Dir(target,
-                    new Assembly(typeof(Rusty.Core).Namespace + ".dll", true),
-                    new Assembly(typeof(Scripting.IACodeProvider).Namespace + ".dll", true),
-                    new WixSharp.File(name + ".exe",
-                        new FileShortcut(name, shortcut),
-                        new FileAssociation(extension) { ContentType = "text/plain", Description= name + " Script", Arguments = "\"%1\" %*", Icon = null }
-                        ),
-                    new WixSharp.File("license.txt"),
-                    docs)
-            };
+                writer.WriteLine("          <Component Id='{0}' Guid='{1}'>", IdL, Guid);
+                writer.WriteLine("            <File Id='{0}' Source='{1}' KeyPath='yes' Assembly='.net' AssemblyManifest='{0}' />", Id, dll.Assembly.Location);
+                writer.WriteLine("          </Component>");
+            }
 
-            project.RegValues = new[]
-            {
-                new RegValue(RegistryHive.ClassesRoot, "." + extension, "PerceivedType", "text")
-            };
+            string exe = Id;
+            writer.WriteLine("          <Component Id='{0}' Guid='{1}'>", IdL, Guid);
+            writer.WriteLine("            <File Id='{0}' Source='{1}'>", exe, main);
+            writer.WriteLine("              <Shortcut Id='{0}' WorkingDirectory='ProgramMenuFolder' Directory='ProgramMenuFolder' Name='{1}.lnk' />", Id, name);
+            writer.WriteLine("            </File>");
+            writer.WriteLine("            <ProgId Id='{0}' Advertise='no' Description='{1} Script'>", Id, name);
+            writer.WriteLine("              <Extension Id='{0}' ContentType='text/plain'>", extension);
+            writer.WriteLine("                <Verb TargetFile='{0}' Id='Open' Command='Open' Argument='&quot;%1&quot; %*' />", exe);
+            writer.WriteLine("                <Verb TargetFile='{0}' Id='Compile' Command='Compile' Argument='/out ! &quot;%1&quot; %*' />", exe); 
+            writer.WriteLine("              </Extension>");
+            writer.WriteLine("            </ProgId>");
+            writer.WriteLine("          </Component>");
 
-            string wixbin = Path.GetFullPath(string.Format("..{0}..{0}WixSharp{0}Wix_bin{0}bin", Path.DirectorySeparatorChar));
-            Compiler.WixLocation = wixbin;
+            writer.WriteLine("          <Component Id='{0}' Guid='{1}'>", IdL, Guid);
+            writer.WriteLine("            <File Id='{0}' Source='{1}' />", Id, license);
+            writer.WriteLine("          </Component>");
 
-            project.WixExtensions.Add("WixNetFxExtension");
-            project.WixNamespaces.Add("xmlns:netfx=\"http://schemas.microsoft.com/wix/NetFxExtension\"");
+            writer.WriteLine("          <Directory Id='INSTALLDIR.{0}' Name='{0}'>", Path.GetFileName(docs));
+            RecurseTree(writer, docs);
+            writer.WriteLine("          </Directory>");
 
-            ext = false;
-            Compiler.WixSourceGenerated += new XDocumentGeneratedDlgt(XmlGenerated);
+            writer.WriteLine("          <Component Id='{0}' Guid='{1}'>", IdL, Guid);
+            writer.WriteLine("            <RegistryKey Root='HKCR' Key='.{0}'>", extension);
+            writer.WriteLine("              <RegistryValue Value='text' Type='string' KeyPath='yes' Name='PerceivedType' />");
+            writer.WriteLine("            </RegistryKey>");
+            writer.WriteLine("          </Component>");
 
-            if (x64)
-                Compiler.CandleOptions += " -arch x64 ";
-            Compiler.LightOptions += " -sw217 ";
+            writer.WriteLine("        </Directory>");
+            writer.WriteLine("      </Directory>");
 
-            Compiler.BuildMsi(project);
+            writer.WriteLine("      <Directory Id='ProgramMenuFolder' Name='ProgramMenuFolder'>");
+            writer.WriteLine("        <Component Id='{0}' Guid='{1}' />", IdL, Guid);
+            writer.WriteLine("      </Directory>");
+            writer.WriteLine("    </Directory>");
+
+            writer.WriteLine("    <Feature Id='{0}' Title='Complete' Absent='allow' Level='1'>", Id);
+            foreach (string item in components)
+                writer.WriteLine("      <ComponentRef Id='{0}' />", item);
+            writer.WriteLine("    </Feature>");
+
+            writer.WriteLine("    <Property Id='WIXUI_INSTALLDIR' Value='INSTALLDIR' />");
+            writer.WriteLine("    <UIRef Id='WixUI_InstallDir' />");
+            writer.WriteLine("    <Icon Id='MainIcon' SourceFile='{0}' />", favicon);
+            writer.WriteLine("    <Property Id='ARPPRODUCTICON' Value='MainIcon' />");
+            writer.WriteLine("    <WixVariable Id='WixUILicenseRtf' Value='{0}' />", licenseRtf);
+            writer.WriteLine("    <Upgrade Id='{0}'>", upgrade);
+            writer.WriteLine("      <UpgradeVersion Minimum='0.0.0.0' IncludeMinimum='yes' Maximum='{0}' IncludeMaximum='no' Property='UPGRADEFOUND' />", version);
+            writer.WriteLine("      <UpgradeVersion Minimum='{0}' IncludeMinimum='no' OnlyDetect='yes' Property='NEWPRODUCTFOUND' />", version);
+            writer.WriteLine("    </Upgrade>");
+            writer.WriteLine("    <CustomAction Id='PreventDowngrading' Error='Newer version already installed' />");
+            writer.WriteLine("    <InstallExecuteSequence>");
+            writer.WriteLine("      <Custom Action='PreventDowngrading' After='FindRelatedProducts'>NEWPRODUCTFOUND</Custom>");
+            writer.WriteLine("      <RemoveExistingProducts After='InstallFinalize' />");
+            writer.WriteLine("    </InstallExecuteSequence>");
+            writer.WriteLine("    <InstallUISequence>");
+            writer.WriteLine("      <Custom Action='PreventDowngrading' After='FindRelatedProducts'>NEWPRODUCTFOUND</Custom>");
+            writer.WriteLine("    </InstallUISequence>");
+
+            writer.WriteLine("  </Product>");
+            writer.WriteLine("</Wix>");
         }
 
-        static Dir RecurseFolder(string parent)
+        static void RecurseTree(TextWriter writer, string root)
         {
-            var entities = new List<WixEntity>();
-
-            foreach (var file in Directory.GetFiles(parent))
-            {
-                if (Path.GetFileName(file) == "index.xml" || Path.GetExtension(file) == "xsl")
-                    continue;
-                entities.Add(new WixSharp.File(file) { Id = n++.ToString() + Path.GetFileName(file) });
-            }
-
-            foreach (var dir in Directory.GetDirectories(parent))
-                entities.Add(RecurseFolder(dir));
-
-            var folder = new Dir(Path.GetFileNameWithoutExtension(parent), entities.ToArray());
-            return folder;
+            RecurseTree(writer, root, root);
         }
 
-        static void XmlGenerated(XDocument document)
+        static void RecurseTree(TextWriter writer, string parent, string root)
         {
-            if (ext)
-                return;
+            int offset = Directory.GetParent(root).FullName.Length + 1;
+            const string sp = "            ";
 
-            foreach (var e in document.Descendants("Extension"))
+            foreach (string dir in Directory.GetDirectories(parent))
             {
-                if (!e.HasElements || !e.HasAttributes)
-                    continue;
-
-                var open = e.Element("Verb");
-                var compile = new XElement(open.Name);
-                compile.Add(open.Attribute("TargetFile"));
-                compile.Add(new XAttribute("Id", "Compile"));
-                compile.Add(new XAttribute("Command", "Compile"));
-                compile.Add(new XAttribute("Argument", "/out ! " + open.Attribute("Argument").Value));
-                e.Add(compile);
-                break;
+                string sub = Path.GetFullPath(dir).Substring(offset).Replace(Path.DirectorySeparatorChar, '.');
+                writer.WriteLine(sp + "<Directory Id='INSTALLDIR.{0}' Name='{1}'>", sub, Path.GetFileName(dir));
+                RecurseTree(writer, dir, root);
+                writer.WriteLine(sp + "</Directory>");
             }
 
-            XNamespace wi = "http://schemas.microsoft.com/wix/2006/wi";
-            foreach (var e in document.Descendants(wi + "Package"))
+            foreach (string file in Directory.GetFiles(parent))
             {
-                if (x64)
-                    e.Add(new XAttribute("Platform", "x64"));
-                e.Add(new XAttribute("AdminImage", "yes"));
+                writer.WriteLine(sp + "<Component Id='{0}' Guid='{1}'>", IdL, Guid);
+                writer.WriteLine(sp + "  <File Id='{0}' Source='{1}' />", Id, file);
+                writer.WriteLine(sp + "</Component>");
             }
+        }
 
-            foreach (var e in document.Descendants("File"))
+        static string Guid
+        {
+            get { return System.Guid.NewGuid().ToString(); }
+        }
+
+        static string Id
+        {
+            get { return "c" + c++.ToString(); }
+        }
+
+        static string IdL
+        {
+            get
             {
-                string id = e.Attribute("Id").Value.Substring("File.".Length);
+                string id = Id;
+                
+                if (components != null)
+                    components.Add(id);
 
-                if (!(id.EndsWith(".dll") || id.EndsWith(".exe")))
-                    continue;
-
-                var netfx = document.Root.GetNamespaceOfPrefix("netfx");
-                var ngen = new XElement(netfx + "NativeImage");
-                ngen.Add(new XAttribute("Id", "ngen_" + id));
-                e.Add(ngen);
+                return id;
             }
-
-            ext = true;
         }
     }
 }
