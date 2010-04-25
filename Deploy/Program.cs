da66b491893bea9cdf819f096b6b42dc390826f3
polyethene <poly@ethene.net>
2010-04-25 19:15:52 +0100

    Added method for zip packaging.

--- Deploy/Program.cs
+++ Deploy/Program.cs
@@ -21,6 +21,28 @@ namespace IronAHK.Setup
             Cleanup();
         }
 
+        static void Zip(string output, string paths, string working)
+        {
+            if (File.Exists(output))
+                File.Delete(output);
+
+            var sz = new Process { StartInfo = new ProcessStartInfo { FileName = "7za", UseShellExecute = false } };
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                string exe = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "7-Zip\\7z.exe");
+
+                if (File.Exists(exe))
+                    sz.StartInfo.FileName = exe;
+            }
+
+            sz.StartInfo.WorkingDirectory = working;
+            sz.StartInfo.Arguments = string.Format("a \"{0}\" \"{1}\" -mx=9", output, paths);
+
+            sz.Start();
+            sz.WaitForExit();
+        }
+
         [Conditional("DEBUG")]
         static void Cleanup()
         {
