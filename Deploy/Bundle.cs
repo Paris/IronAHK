8d5164a28ddb937ed285c4c9e79ad3493063b7a3
A <inspiration3@gmail.com>
2010-04-25 21:45:48 +0100

    Generate OSX app bundle.

--- Deploy/Bundle.cs
+++ Deploy/Bundle.cs
@@ -1,11 +1,53 @@
-﻿
+﻿using System.IO;
+
 namespace IronAHK.Setup
 {
     partial class Program
     {
         static void AppBundle()
         {
-            // TODO: create OSX app bundle
+            string name = Name + ".app";
+            string zip = Path.Combine(Output, string.Format("{0}-{1}.zip", name, Version));
+            string tmp = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            string root = Path.Combine(tmp, name);
+            string res = Path.Combine(Path.Combine(root, "Contents"), "Resources");
+            string favicon = string.Format("..{0}..{0}..{0}{1}{0}Site{0}favicon.ico", Path.DirectorySeparatorChar.ToString(), Name);
+
+            if (!Directory.Exists(res))
+                Directory.CreateDirectory(res);
+
+            foreach (var dll in new[] { typeof(Rusty.Core), typeof(Scripting.IACodeProvider) })
+                File.Copy(dll.Assembly.Location, Path.Combine(res, Path.GetFileName(dll.Assembly.Location)));
+
+            foreach (var path in new[] { favicon, Name + ".exe" })
+                File.Copy(path, Path.Combine(res, Path.GetFileName(path)));
+
+            string osx = Path.Combine(root, "MacOS");
+
+            if (!Directory.Exists(osx))
+                Directory.CreateDirectory(osx);
+
+            const string sh = "MacOS.sh";
+            File.Copy(sh, Path.Combine(osx, Name));
+
+            var writer = new StreamWriter(Path.Combine(root, "Info.plist")) { NewLine = "\n" };
+            writer.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            writer.WriteLine("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
+            writer.WriteLine("<plist version=\"1.0\">");
+            writer.WriteLine("<dict>");
+            writer.WriteLine("  <key>CFBundleIdentifier</key>");
+            writer.WriteLine("  <string>{0}</string>", Name);
+            writer.WriteLine("  <key>CFBundleExecutable</key>");
+            writer.WriteLine("  <string>{0}</string>", Name);
+            writer.WriteLine("  <key>CFBundleIconFile</key>");
+            writer.WriteLine("  <string>{0}</string>", Path.GetFileName(favicon));
+            writer.WriteLine("</dict>");
+            writer.WriteLine("</plist>");
+            writer.Close();
+
+            Zip(zip, name, tmp);
+
+            Directory.Delete(tmp, true);
         }
     }
 }
