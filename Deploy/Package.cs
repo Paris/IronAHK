97a084f7939cc4f9603626c9304f2c991b57c28e
A <inspiration3@gmail.com>
2010-04-25 19:15:52 +0100

    Added method for zip packaging.

--- Deploy/Package.cs
+++ Deploy/Package.cs
@@ -1,11 +1,26 @@
-﻿
+﻿using System.IO;
+
 namespace IronAHK.Setup
 {
     partial class Program
     {
         static void PackageZip()
         {
-            // TODO: create binary zip packages
+            string zip = Path.Combine(Output, string.Format("{0}-{1}.zip", Name, Version));
+            string tmp = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()), target = Path.Combine(tmp, Name);
+
+            if (!Directory.Exists(target))
+                Directory.CreateDirectory(target);
+
+            foreach (var dll in new[] { typeof(Rusty.Core), typeof(Scripting.IACodeProvider) })
+                File.Copy(dll.Assembly.Location, Path.Combine(target, Path.GetFileName(dll.Assembly.Location)));
+
+            foreach (var file in new[] { "license.txt", "Example.ahk", "setup.sh", Name + ".exe" })
+                File.Copy(file, Path.Combine(target, file));
+
+            Zip(zip, Path.GetFileName(target), tmp);
+
+            Directory.Delete(tmp, true);
         }
     }
 }
