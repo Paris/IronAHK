41c017f97e519cdc9a00cf46eab1f8b1ebfc5ce0
A <inspiration3@gmail.com>
2010-05-12 20:30:03 +0100

    Moved menu method to separated file.

---
+++ Rusty/Core/Gui/Menu.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IronAHK.Rusty
+{
+    partial class Core
+    {
+        /// <summary>
+        /// Creates, deletes, modifies and displays menus and menu items.
+        /// </summary>
+        /// <param name="MenuName"></param>
+        /// <param name="Cmd"></param>
+        /// <param name="P3"></param>
+        /// <param name="P4"></param>
+        /// <param name="P5"></param>
+        public static void Menu(string MenuName, string Cmd, string P3, string P4, string P5)
+        {
+
+        }
+    }
+}
