6de06ab3d66ff53c4628e8caf0d79db5643445df
A <inspiration3@gmail.com>
2010-06-17 23:15:45 +0100

    Recognise binary operator for unary operator checks.

--- Scripting/Parser/Construct/Expression.cs
+++ Scripting/Parser/Construct/Expression.cs
@@ -484,8 +484,9 @@ namespace IronAHK.Scripting
 
             for (int i = 1; i < parts.Count; i++)
             {
-                if (parts[i] is Script.Operator && (parts[i - 1] is Script.Operator || IsVarAssignment(parts[i - 1])) && IsUnaryOperator((Script.Operator)parts[i]) &&
-                    (i + 1 < parts.Count ? !(parts[i + 1] is CodePrimitiveExpression) : true))
+                if (parts[i] is Script.Operator &&
+                    (parts[i - 1] is Script.Operator || parts[i - 1] as CodeBinaryOperatorType? == CodeBinaryOperatorType.Assign || IsVarAssignment(parts[i - 1])) &&
+                    IsUnaryOperator((Script.Operator)parts[i]))
                 {
                     int n = i + 1, m = n + 1;
 
