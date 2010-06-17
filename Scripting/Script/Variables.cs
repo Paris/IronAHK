ce90cee6bee3b12f47fa8c29866e553b15a9054d
polyethene <poly@ethene.net>
2010-06-17 08:46:19 +0100

    Moved reserved variable handling to new class.

--- Scripting/Script/Variables.cs
+++ Scripting/Script/Variables.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace IronAHK.Scripting
 {
@@ -15,32 +17,43 @@ namespace IronAHK.Scripting
             {
                 get
                 {
-                    if (!table.ContainsKey(key))
-                        return null;
+                    lock (table)
+                    {
+                        if (!table.ContainsKey(key))
+                            return GetReservedVariable(key);
 
-                    if (AutoMark && !collect.Contains(key))
-                        collect.Push(key);
+                        if (AutoMark && !collect.Contains(key))
+                            collect.Push(key);
 
-                    return table[key];
+                        return table[key];
+                    }
                 }
                 set
                 {
-                    var exists = table.ContainsKey(key);
+                    if (SetReservedVariable(key, value))
+                        return;
 
-                    if (value == null)
+                    lock (table)
                     {
-                        if (exists)
-                            table.Remove(key);
-                    }
-                    else
-                    {
-                        if (exists)
-                            table[key] = value;
+                        var exists = table.ContainsKey(key);
+
+                        if (value == null)
+                        {
+                            if (exists)
+                                table.Remove(key);
+                        }
                         else
-                            table.Add(key, value);
+                        {
+                            if (exists)
+                                table[key] = value;
+                            else
+                                table.Add(key, value);
+                        }
                     }
                 }
             }
+
+            #region Collection
 
             public void Mark(params string[] keys)
             {
@@ -54,6 +67,54 @@ namespace IronAHK.Scripting
                 while (collect.Count != 0)
                     this[collect.Pop()] = null;
             }
+
+            #endregion
+
+            #region Reserved
+
+            static bool SetReservedVariable(string name, object value)
+            {
+                var prop = FindReservedVariable(name);
+                var set = prop != null && prop.CanWrite;
+
+                if (set)
+                    prop.SetValue(null, value, null);
+
+                return set;
+            }
+
+            static object GetReservedVariable(string name)
+            {
+                var prop = FindReservedVariable(name);
+                return prop == null || !prop.CanRead ? null : prop.GetValue(null, null);
+            }
+
+            static PropertyInfo FindReservedVariable(string name)
+            {
+                const string A_ = "A_";
+                int z = name.LastIndexOf('.');
+
+                if (z != -1)
+                {
+                    z++;
+                    if (z + A_.Length > name.Length)
+                        return null;
+                    name = name.Substring(z);
+                }
+
+                if (!name.Substring(0, A_.Length).Equals(A_, StringComparison.OrdinalIgnoreCase))
+                    return null;
+
+                PropertyInfo prop = null;
+
+                foreach (var item in typeof(Script).BaseType.GetProperties())
+                    if (item.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                        prop = item;
+
+                return prop;
+            }
+
+            #endregion
         }
     }
 }
