6e7aa31cfabc71ec8fbf7a569910248bdc870f5a
A <inspiration3@gmail.com>
2010-06-24 22:18:18 +0100

    Added NUnit test for SubStr.

--- Tests/Rusty/String.cs
+++ Tests/Rusty/String.cs
@@ -1,9 +1,21 @@
+using IronAHK.Rusty;
+using NUnit.Framework;
+
 namespace IronAHK.Tests
 {
     partial class Rusty
     {
         // TODO: string tests
 
+        [Test, Category("String")]
+        public void SubStr()
+        {
+            const string subject = "abcdefghi";
 
+            Assert.AreEqual("i", Core.SubStr(subject, 0, 0));
+            Assert.AreEqual("bcd", Core.SubStr(subject, 2, 3));
+            Assert.AreEqual("fghi", Core.SubStr(subject, -3, 0));
+            Assert.AreEqual("efg", Core.SubStr(subject, -4, -2));
+        }
     }
 }
