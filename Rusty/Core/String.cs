e79bd09b75bcb04ee87ccd6cf66bb1994ef34c56
polyethene <poly@ethene.net>
2010-06-27 06:08:42 +0100

    Moved obsolete commands.

--- Rusty/Core/String.cs
+++ Rusty/Core/String.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace IronAHK.Rusty
 {
@@ -597,6 +599,128 @@ namespace IronAHK.Rusty
 
             length = Math.Min(length, d);
             return input.Substring(index, length);
+        }
+
+        /// <summary>
+        /// Performs miscellaneous math functions, bitwise operations, and tasks such as ASCII to Unicode conversion.
+        /// This function is obsolete, please use the related newer syntax.
+        /// <seealso cref="Asc"/>
+        /// <seealso cref="Chr"/>
+        /// <seealso cref="Mod"/>
+        /// <seealso cref="Exp"/>
+        /// <seealso cref="Sqrt"/>
+        /// <seealso cref="Log"/>
+        /// <seealso cref="Ln"/>
+        /// <seealso cref="Round"/>
+        /// <seealso cref="Ceil"/>
+        /// <seealso cref="Floor"/>
+        /// <seealso cref="Abs"/>
+        /// <seealso cref="Sin"/>
+        /// <seealso cref="Cos"/>
+        /// <seealso cref="Tan"/>
+        /// <seealso cref="ASin"/>
+        /// <seealso cref="ACos"/>
+        /// <seealso cref="ATan"/>
+        /// <seealso cref="Floor"/>
+        /// <seealso cref="Floor"/>
+        /// </summary>
+        [Obsolete, Conditional("LEGACY")]
+        public static void Transform(ref string OutputVar, string Cmd, string Value1, string Value2)
+        {
+            OutputVar = string.Empty;
+            switch (Cmd.Trim().ToLowerInvariant())
+            {
+                case Keyword_Unicode:
+                    if (Value1 == null)
+                        OutputVar = Clipboard.GetText();
+                    else OutputVar = Value1;
+                    break;
+                case Keyword_Asc:
+                    OutputVar = char.GetNumericValue(Value1, 0).ToString();
+                    break;
+                case Keyword_Chr:
+                    OutputVar = char.ConvertFromUtf32(int.Parse(Value1));
+                    break;
+                case Keyword_Deref:
+                    // TODO: dereference transform
+                    break;
+                case "html":
+                    OutputVar = Value1
+                        .Replace("\"", "&quot;")
+                        .Replace("&", "&amp;")
+                        .Replace("<", "&lt;")
+                        .Replace(">", "&gt;")
+                        .Replace("\n", "<br/>\n");
+                    break;
+                case Keyword_Mod:
+                    OutputVar = (double.Parse(Value1) % double.Parse(Value2)).ToString();
+                    break;
+                case Keyword_Pow:
+                    OutputVar = Math.Pow(double.Parse(Value1), double.Parse(Value2)).ToString();
+                    break;
+                case Keyword_Exp:
+                    OutputVar = Math.Pow(double.Parse(Value1), Math.E).ToString();
+                    break;
+                case Keyword_Sqrt:
+                    OutputVar = Math.Sqrt(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Log:
+                    OutputVar = Math.Log10(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Ln:
+                    OutputVar = Math.Log(double.Parse(Value1), Math.E).ToString();
+                    break;
+                case Keyword_Round:
+                    int p = int.Parse(Value2);
+                    OutputVar = Math.Round(double.Parse(Value1), p == 0 ? 1 : p).ToString();
+                    break;
+                case Keyword_Ceil:
+                    OutputVar = Math.Ceiling(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Floor:
+                    OutputVar = Math.Floor(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Abs:
+                    double d = double.Parse(Value1);
+                    OutputVar = (d < 0 ? d * -1 : d).ToString();
+                    break;
+                case Keyword_Sin:
+                    OutputVar = Math.Sin(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Cos:
+                    OutputVar = Math.Cos(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Tan:
+                    OutputVar = Math.Tan(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Asin:
+                    OutputVar = Math.Asin(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Acos:
+                    OutputVar = Math.Acos(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_Atan:
+                    OutputVar = Math.Atan(double.Parse(Value1)).ToString();
+                    break;
+                case Keyword_BitNot:
+                    OutputVar = (~int.Parse(Value1)).ToString();
+                    break;
+                case Keyword_BitAnd:
+                    OutputVar = (int.Parse(Value1) & int.Parse(Value2)).ToString();
+                    break;
+                case Keyword_BitOr:
+                    OutputVar = (int.Parse(Value1) | int.Parse(Value2)).ToString();
+                    break;
+                case Keyword_BitXor:
+                    OutputVar = (int.Parse(Value1) ^ int.Parse(Value2)).ToString();
+                    break;
+                case Keyword_BitShiftLeft:
+                    OutputVar = (int.Parse(Value1) << int.Parse(Value2)).ToString();
+                    break;
+                case Keyword_BitShiftRight:
+                    OutputVar = (int.Parse(Value1) >> int.Parse(Value2)).ToString();
+                    break;
+            }
         }
     }
 }
