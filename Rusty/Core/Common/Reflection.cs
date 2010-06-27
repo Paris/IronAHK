a0e6da3ab4bdd1e186038e7b0e56143bb0a0ea70
A <inspiration3@gmail.com>
2010-06-27 07:37:58 +0100

    Organised Formats.cs.

---
+++ Rusty/Core/Common/Reflection.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace IronAHK.Rusty
+{
+    partial class Core
+    {
+        static object SafeInvoke(string name, params object[] args)
+        {
+            var method = FindLocalRoutine(name);
+
+            if (method == null)
+                return null;
+
+            object value = null;
+
+            try { value = method.Invoke(null, new object[] { args }); }
+            catch (Exception) { }
+            
+            return value;
+        }
+
+        static void SafeSetProperty(object item, string name, object value)
+        {
+            var prop = item.GetType().GetProperty(name, value.GetType());
+
+            if (prop == null)
+                return;
+
+            prop.SetValue(item, value, null);
+        }
+
+        static MethodInfo FindLocalRoutine(string name)
+        {
+            return FindLocalMethod(LabelMethodName(name));
+        }
+
+        static MethodInfo FindLocalMethod(string name)
+        {
+            var stack = new StackTrace(false).GetFrames();
+
+            for (int i = 0; i < stack.Length; i++)
+            {
+                var type = stack[i].GetMethod().DeclaringType;
+
+                if (type == typeof(Core))
+                    continue;
+
+                var list = type.GetMethods();
+
+                for (int z = 0; z < list.Length; z++)
+                    if (list[z].Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                        return list[z];
+            }
+
+            return null;
+        }
+
+        static string LabelMethodName(string raw)
+        {
+            foreach (var sym in raw)
+            {
+                if (!char.IsLetterOrDigit(sym))
+                    return string.Concat("label_", raw.GetHashCode().ToString("X"));
+            }
+            return raw;
+        }
+    }
+}
