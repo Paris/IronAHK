8c915d0028b907673b70839d3cc199e55b491c1c
A <inspiration3@gmail.com>
2010-08-12 22:23:22 +0100

    TV_GetNext method.

--- Rusty/Core/Common/Keywords.cs
+++ Rusty/Core/Common/Keywords.cs
@@ -344,6 +344,7 @@ namespace IronAHK.Rusty
         const string Keyword_ARGB = "argb";
         const string Keyword_Variation = "variation";
         const string Keyword_Trans = "trans";
+        const string Keyword_Full = "full";
 
         const char Keyword_ModifierWin = '#';
         const char Keyword_ModifierAlt = '!';
