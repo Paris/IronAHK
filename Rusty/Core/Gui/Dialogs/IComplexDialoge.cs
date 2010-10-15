04ebcb8dc61f24696152ea6684373ff107651bad
IsNull <IsNull@securityvision.ch>
2010-10-15 13:21:08 +0200

    Define common Complex dialog interface

---
+++ Rusty/Core/Gui/Dialogs/IComplexDialoge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronAHK.Rusty
+{
+    interface IComplexDialoge
+    {
+        string MainText { get; set; }
+        string Subtext { get; set; }
+        string Title { get; set; }
+
+        #region Form
+
+        //DialogResult ShowDialog();
+        void Show();
+        void Close();
+        void Dispose();
+
+        bool Visible { get; set; }
+        bool TopMost { get; set; }
+
+        #region Invokable
+
+        bool InvokeRequired { get; }
+        object Invoke(Delegate Method, params object[] obj);
+        object Invoke(Delegate Method);
+
+        #endregion
+
+        #endregion
+    }
+}
