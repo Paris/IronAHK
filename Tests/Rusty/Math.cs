abcd8ef622e751f1d771481a1ff7dacd383f8c92
A <inspiration3@gmail.com>
2010-10-20 01:23:21 +0100

    Removed bad trignometric test code.

--- Tests/Rusty/Math.cs
+++ Tests/Rusty/Math.cs
@@ -16,43 +16,5 @@ namespace IronAHK.Tests
             Assert.AreEqual(9.81, Core.Abs(-9.81m));
             Assert.AreEqual(0, Core.Abs(-0));
         }
-
-        [Test, Category("Math")]
-        public void Trigonometry()
-        {
-            foreach (var d in new[] { 0m, 45m, 90m, 271.3m, (decimal)Math.PI * -10 })
-            {
-                string s;
-
-                s = d.ToString();
-                Assert.AreEqual((decimal)Math.Sin((double)d), Core.Sin(d), "sin " + s);
-                Assert.AreEqual((decimal)Math.Cos((double)d), Core.Cos(d), "cos " + s);
-                Assert.AreEqual((decimal)Math.Tan((double)d), Core.Tan(d), "tan " + s);
-
-                decimal r = d * ((decimal)Math.PI / 180);
-                s = r.ToString();
-                if (r < -1 || r > 1)
-                {
-                    try
-                    {
-                        Core.ASin(r);
-                        throw new ArithmeticException();
-                    }
-                    catch (Exception e) { Assert.IsTrue(e is OverflowException, "asin " + s); }
-                    try
-                    {
-                        Core.ACos(r);
-                        throw new ArithmeticException();
-                    }
-                    catch (Exception e) { Assert.IsTrue(e is OverflowException, "acos " + s); }
-                }
-                else
-                {
-                    Assert.AreEqual((decimal)Math.Asin((double)r), Core.ASin(r), "asin " + s);
-                    Assert.AreEqual((decimal)Math.Acos((double)r), Core.ACos(r), "acos " + s);
-                }
-                Assert.AreEqual((decimal)Math.Atan((double)r), Core.ATan(r), "atan " + s);
-            }
-        }
     }
 }
