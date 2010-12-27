61d1288eac529058defae05ad07be759856255e8
IsNull <IsNull@securityvision.ch>
2010-12-27 14:18:57 +0100

    encapsulated mapping logic

---
+++ Rusty/Cores/Common/Mapper/MapperBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronAHK.Rusty.Cores.Common.Mapper
+{
+    internal class MapperBase<T> where T : struct, IConvertible
+    {
+        static protected Dictionary<T, string> clrMappingTable = new Dictionary<T, string>();
+
+        public MapperBase() {
+            SetUpMappingTable();
+        }
+
+        public virtual T? LookUpCLRType(string keyword) {
+            T? res = null;
+            foreach(var kv in clrMappingTable) {
+                if(kv.Value == keyword) {
+                    res = kv.Key;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        public virtual void SetUpMappingTable() {
+            //
+        }
+
+        public virtual string LookUpIAType(T clrType) {
+            if(clrMappingTable.ContainsKey(clrType))
+                return clrMappingTable[clrType];
+            else
+                return "";
+        }
+    }
+}
---
+++ Rusty/Cores/Common/Mapper/MappingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronAHK.Rusty.Cores.Common.Mapper
+{
+    public sealed class MappingService
+    {
+        static readonly MappingService instance = new MappingService(); 
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static MappingService() { }
+
+        MappingService() {
+            // add here all mapping providers here
+            DriveType = new DriveTypeMapper();
+        }
+
+
+        internal readonly DriveTypeMapper DriveType;
+
+        public static MappingService Instance {
+            get { return instance; }
+        }
+    }
+}
