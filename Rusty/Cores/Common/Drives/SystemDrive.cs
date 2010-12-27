7fbeaf7c958fab9e56eaf6a4ad8e7cabc64f78b9
IsNull <IsNull@securityvision.ch>
2010-12-27 15:10:16 +0100

    abstracted Drive Functions which can't be done the manged way

---
+++ Rusty/Cores/Common/Drives/SystemDrive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IronAHK.Rusty.Cores.Common.Drives
+{
+    internal enum StatusCD
+    {
+        NotReady,
+        Open,
+        Playing,
+        Paused,
+        Seeking,
+        Stopped
+    }
+
+    internal abstract class SystemDrive
+    {
+        protected DriveInfo drive;
+
+        public SystemDrive(DriveInfo udrive) {
+            drive = udrive;
+        }
+
+        public bool IsCDDrive() {
+            try {
+                return (drive.DriveType == DriveType.CDRom);
+            } catch {
+                return false;
+            }
+        }
+
+        public abstract void Eject();
+
+        public abstract void Retract();
+
+        public abstract StatusCD Status { get; }
+
+        public abstract long Serial { get; }
+    }
+}
---
+++ Rusty/Linux/Drives/SystemDriveLinux.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IronAHK.Rusty.Cores.Common.Drives;
+using System.IO;
+
+namespace IronAHK.Rusty.Linux.Drives
+{
+    public class SystemDriveLinux : SystemDrive
+    {
+        public SystemDriveLinux(DriveInfo drv)
+            : base(drv) { }
+
+        public override void Eject() {
+            throw new NotImplementedException();
+        }
+
+        public override void Retract() {
+            throw new NotImplementedException();
+        }
+
+        public override StatusCD Status {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override long Serial {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
