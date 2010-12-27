4c46729f8efe60369b3c948f68fc09c14ff9dee5
IsNull <IsNull@securityvision.ch>
2010-12-27 15:10:16 +0100

    abstracted Drive Functions which can't be done the manged way

---
+++ Rusty/Cores/Common/Drives/SystemDriveFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using IronAHK.Rusty.Linux.Drives;
+using IronAHK.Rusty.Windows.Drives;
+
+namespace IronAHK.Rusty.Cores.Common.Drives
+{
+    internal static class SystemDriveFactory
+    {
+        public static SystemDrive CreateSystemDrive(DriveInfo drive) {
+            if(Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return new SystemDriveWindows(drive);
+            else
+                return new SystemDriveLinux(drive);
+        }
+    }
+}
