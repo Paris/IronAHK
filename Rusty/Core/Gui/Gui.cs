9d1fd68ad4bb92b78c31e98527009979353dc732
Julien <Julien.k@gmx.net>
2011-02-02 13:21:38 -0800

    If the Gui is Colored (Gui,Color is buggy !) and a Control is designed, it inherit the BackColor propertie. to prevent that it will be overwritten with Color.Transparent !
    &
    
    The ForeColor propertie have not to be changed
    by gui,color,

--- Rusty/Core/Gui/Gui.cs
+++ Rusty/Core/Gui/Gui.cs
@@ -200,8 +200,7 @@ namespace IronAHK.Rusty
                     break;
 
                 case Keyword_Color:
-                    guis[id].BackColor = Keyword_Default.Equals(Param2, StringComparison.OrdinalIgnoreCase) ? Color.Transparent : ParseColor(Param2);
-                    guis[id].ForeColor = Keyword_Default.Equals(Param3, StringComparison.OrdinalIgnoreCase) ? Color.Transparent : ParseColor(Param3);
+                    guis[id].BackColor = Keyword_Default.Equals(Param2, StringComparison.OrdinalIgnoreCase) ? Color.Transparent : ParseColor(Param2); // <--Buggy
                     break;
 
                 case Keyword_Margin:
@@ -1230,6 +1229,8 @@ namespace IronAHK.Rusty
 
             if (opts == null)
                 GuiApplyStyles(control, options);
+
+            control.BackColor = Color.Transparent; //Overwrite the inherited BackColor propertie from gui[id]
 
             if (control is Button)
             {
