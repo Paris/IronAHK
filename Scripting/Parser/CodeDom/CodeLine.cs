f9c2c3419e16c3f34406d3995229fe32e5fa0fd7
IsNull <IsNull@securityvision.ch>
2011-04-07 20:40:31 +0200

    commit before reorganisation changes

--- Scripting/Parser/CodeDom/CodeLine.cs
+++ Scripting/Parser/CodeDom/CodeLine.cs
@@ -11,5 +11,9 @@ namespace IronAHK.Scripting
         }
 
         public string Code { get; set; }
+
+        public override string ToString() {
+            return string.Format("{0}: {1}", LineNumber, Code);
+        }
     }
 }
